ecfcecdf94dc5fa4ac8a9734b189d8e47a20b173
Marcelo Villa-Piñeros <mvilla@humboldt.org.co>
2021-10-13 12:39:37 -0500

    Import new functions.

--- wiutils/__init__.py
+++ wiutils/__init__.py
@@ -1,1 +1,7 @@
-from wiutils.tables import compute_hill_numbers, remove_duplicates
+from wiutils.io import read_project
+from wiutils.tables import (
+    compute_detection_history,
+    compute_hill_numbers,
+    remove_duplicates,
+    remove_unidentified,
+)
