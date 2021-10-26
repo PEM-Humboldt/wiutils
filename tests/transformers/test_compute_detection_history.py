4759cc9d37afaf3b7720724f1f1c505749500b86
Marcelo Villa-Piñeros <mvilla@humboldt.org.co>
2021-10-26 12:34:46 -0500

    Add unit tests for the compute_detection_history function.

---
+++ tests/transformers/test_compute_detection_history.py
@@ -0,0 +1,241 @@
+"""
+Test cases for the wiutils.transformers.compute_detection_history function.
+"""
+import numpy as np
+import pandas as pd
+import pytest
+
+from wiutils.transformers import compute_detection_history
+
+
+@pytest.fixture(scope="function")
+def images():
+    return pd.DataFrame(
+        {
+            "deployment_id": [
+                "001",
+                "001",
+                "001",
+                "001",
+                "001",
+                "001",
+                "001",
+                "001",
+                "002",
+                "002",
+                "002"
+            ],
+            "scientific_name": [
+                "Panthera onca",
+                "Panthera onca",
+                "Panthera onca",
+                "Panthera onca",
+                "Panthera onca",
+                "Leopardus pardalis",
+                "Leopardus pardalis",
+                "Leopardus pardalis",
+                "Panthera onca",
+                "Panthera onca",
+                "Panthera onca"
+            ],
+            "timestamp": [
+                "2020-11-27 06:45:57",
+                "2020-11-29 09:24:32",
+                "2020-12-04 14:41:52",
+                "2020-12-05 16:17:41",
+                "2020-12-06 08:51:01",
+                "2020-11-25 09:58:12",
+                "2020-12-01 10:11:25",
+                "2020-12-05 17:21:33",
+                "2020-11-30 09:42:29",
+                "2020-12-03 11:14:32",
+                "2020-12-11 07:53:07"
+            ],
+        }
+    )
+
+
+@pytest.fixture(scope="function")
+def deployments():
+    return pd.DataFrame(
+        {
+            "deployment_id": ["001", "002"],
+            "start_date": ["2020-11-24", "2020-11-27"],
+            "end_date": ["2020-12-07", "2020-12-14"]
+        }
+    )
+
+
+@pytest.fixture(scope="module")
+def column_names():
+    return {
+        "date_col": "timestamp",
+        "site_col": "deployment_id",
+        "species_col": "scientific_name",
+        "start_col": "start_date",
+        "end_col": "end_date",
+    }
+
+
+def test_compute_abundance(images, deployments, column_names):
+    result = compute_detection_history(images, deployments, days=7, **column_names)
+    expected = pd.DataFrame(
+        {
+            "scientific_name": [
+                "Leopardus pardalis",
+                "Leopardus pardalis",
+                "Leopardus pardalis",
+                "Leopardus pardalis",
+                "Leopardus pardalis",
+                "Leopardus pardalis",
+                "Panthera onca",
+                "Panthera onca",
+                "Panthera onca",
+                "Panthera onca",
+                "Panthera onca",
+                "Panthera onca"
+            ],
+            "deployment_id": [
+                "001",
+                "001",
+                "001",
+                "002",
+                "002",
+                "002",
+                "001",
+                "001",
+                "001",
+                "002",
+                "002",
+                "002",
+            ],
+            "timestamp": pd.to_datetime([
+                "2020-11-24",
+                "2020-12-01",
+                "2020-12-08",
+                "2020-11-24",
+                "2020-12-01",
+                "2020-12-08",
+                "2020-11-24",
+                "2020-12-01",
+                "2020-12-08",
+                "2020-11-24",
+                "2020-12-01",
+                "2020-12-08"
+            ]),
+            "value": [1, 2, np.nan, 0, 0, 0, 2, 3, np.nan, 1, 1, 1],
+        }
+    )
+    pd.testing.assert_frame_equal(result, expected)
+
+
+def test_compute_presence(images, deployments, column_names):
+    result = compute_detection_history(
+        images, deployments, days=14, compute_abundance=False, **column_names
+    )
+    expected = pd.DataFrame(
+        {
+            "scientific_name": [
+                "Leopardus pardalis",
+                "Leopardus pardalis",
+                "Leopardus pardalis",
+                "Leopardus pardalis",
+                "Panthera onca",
+                "Panthera onca",
+                "Panthera onca",
+                "Panthera onca"
+            ],
+            "deployment_id": [
+                "001",
+                "001",
+                "002",
+                "002",
+                "001",
+                "001",
+                "002",
+                "002",
+            ],
+            "timestamp": pd.to_datetime([
+                "2020-11-24",
+                "2020-12-08",
+                "2020-11-24",
+                "2020-12-08",
+                "2020-11-24",
+                "2020-12-08",
+                "2020-11-24",
+                "2020-12-08"
+            ]),
+            "value": [1, np.nan, 0, 0, 1, np.nan, 1, 1],
+        }
+    )
+    pd.testing.assert_frame_equal(result, expected)
+
+
+def test_date_range_images(images, deployments, column_names):
+    result = compute_detection_history(
+        images, deployments, date_range="images", days=15, **column_names
+    )
+    expected = pd.DataFrame(
+        {
+            "scientific_name": [
+                "Leopardus pardalis",
+                "Leopardus pardalis",
+                "Leopardus pardalis",
+                "Leopardus pardalis",
+                "Panthera onca",
+                "Panthera onca",
+                "Panthera onca",
+                "Panthera onca"
+            ],
+            "deployment_id": [
+                "001",
+                "001",
+                "002",
+                "002",
+                "001",
+                "001",
+                "002",
+                "002",
+            ],
+            "timestamp": pd.to_datetime([
+                "2020-11-25",
+                "2020-12-10",
+                "2020-11-25",
+                "2020-12-10",
+                "2020-11-25",
+                "2020-12-10",
+                "2020-11-25",
+                "2020-12-10"
+            ]),
+            "value": [3, np.nan, 0, 0, 5, np.nan, 2, 1],
+        }
+    )
+    pd.testing.assert_frame_equal(result, expected)
+
+
+def test_pivot(images, deployments, column_names):
+    result = compute_detection_history(
+        images, deployments, date_range="images", days=15, pivot=True, **column_names
+    )
+    expected = pd.DataFrame(
+        {
+            "scientific_name": [
+                "Leopardus pardalis",
+                "Leopardus pardalis",
+                "Panthera onca",
+                "Panthera onca",
+            ],
+            "deployment_id": ["001", "002", "001", "002"],
+            "2020-11-25": [3.0, 0.0, 5.0, 2.0],
+            "2020-12-10": [np.nan, 0, np.nan, 1]
+        }
+    )
+    pd.testing.assert_frame_equal(result, expected)
+
+
+def test_intact_input(images, deployments, column_names):
+    images_original = images.copy()
+    deployments_original = deployments.copy()
+    compute_detection_history(images, deployments, days=7, **column_names)
+    pd.testing.assert_frame_equal(images_original, images)
+    pd.testing.assert_frame_equal(deployments_original, deployments)
