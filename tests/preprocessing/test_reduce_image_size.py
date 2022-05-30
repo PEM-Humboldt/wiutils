a5417a6ad0bff3f749bbef98c1de8858b527fcab
Marcelo Villa-Piñeros <mvilla@humboldt.org.co>
2022-05-30 16:03:48 -0500

    Add helper function to patch image.

--- tests/preprocessing/test_reduce_image_size.py
+++ tests/preprocessing/test_reduce_image_size.py
@@ -6,24 +6,27 @@ import PIL.Image
 from wiutils.preprocessing import reduce_image_size
 
 
-def test_dimensions(mocker):
+def patch_image(mocker):
     mocker.patch("PIL.Image.open")
     image = PIL.Image.open()
     mocker.patch.object(image, "height", 60)
     mocker.patch.object(image, "width", 80)
     mocker.patch.object(image, "resize")
+    return image
+
+
+def test_dimensions(mocker):
+    image = patch_image(mocker)
     reduce_image_size("image.jpg", "resized.jpg", factor=0.9, method=0)
     image.resize.assert_called_once_with((72, 54), 0)
 
 
 def test_intact_exif(mocker):
-    mocker.patch("PIL.Image.open")
-    image = PIL.Image.open()
+    image = patch_image(mocker)
     mocker.patch.object(image, "format", "JPEG")
     mocker.patch.object(image, "getexif", return_value={306: "2017:04:21 23:11:08"})
-    mocker.patch.object(image, "resize")
-    result = image.resize()
     mocker.patch.object(image, "save")
+    result = image.resize()
     reduce_image_size("image.jpg", "resized.jpg")
     result.save.assert_called_once_with(
         "resized.jpg", format="JPEG", exif={306: "2017:04:21 23:11:08"}
